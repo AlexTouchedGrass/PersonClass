e4395934c5f8264cbaa29cca7a8f30326b35b7c0
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-12-12 09:46:24 -0500

    Words

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //Person 1
+        String p1FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's First Name:");
+        String p1lastName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's Last Name:");
+        int p1Age = InputHelper.getRangedInt(scan, "Get Person 1's Age: [1-99]", 1, 99);
+
+        //Person 2
+        String p2FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's First Name:");
+        String p2lastName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's Last Name:");
+        int p2Age = InputHelper.getRangedInt(scan, "Get Person 2's Age: [1-99]", 1, 99);
+
+        //Creating Person Objects
+        Person p1 = new Person(p1FirstName, p1lastName, p1Age);
+        Person p2 = new Person(p2FirstName, p2lastName, p2Age);
+
+        //Displaying the info using toString
+        System.out.println("Person 1 Info\n" + p1);
+        System.out.println("Person 2 Info\n" + p2);
+
+        //Updating Info
+        p2.setLastName("Noob");
+        p1.setAge(32);
+
+        //Redisplaying Info
+        System.out.println("Person 1 Info\n" + p1);
+        System.out.println("Person 2 Info\n" + p2);
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Person {
+
+    private String lastName;
+    private String firstName;
+    private int age;
+
+    //Constructor
+    public Person (String firstName, String lastName, int age){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String toString() {
+        return String.format("First name: %s Last Name: %s Age: %d", this.firstName, this.lastName, this.age);
+    }
+
+    //Accessors
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+    //Mutators
+    public void setFirstname(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age){
+        this.age = age;
     }
 }
