17a75ccf6cc0cf8363a37a96218ce7c501f19b78
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-12-14 09:46:20 -0500

    Words cannot describe my agony.

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //Person 1
-        String p1FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's First Name:");
-        String p1lastName = InputHelper.getNonZeroLenString(scan, "Enter Person 1's Last Name:");
-        int p1Age = InputHelper.getRangedInt(scan, "Get Person 1's Age: [1-99]", 1, 99);
+        String p1FirstName = "P";
 
         //Person 2
         String p2FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's First Name:");
-        String p2lastName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's Last Name:");
-        int p2Age = InputHelper.getRangedInt(scan, "Get Person 2's Age: [1-99]", 1, 99);
+        String p2LastName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's Last Name:");
+        int p2X = InputHelper.getRangedInt(scan, "Enter Person 2's X Coordinate:",0,1000);
+        int p2Y = InputHelper.getRangedInt(scan, "Enter Person 2's Y Coordinate:",0,100);
+        int p2Z = InputHelper.getRangedInt(scan, "Enter Person 2's Z Coordinate:",0,1000);
+
+        //Person 3
+        String p3FirstName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's First Name:");
+        String p3LastName = InputHelper.getNonZeroLenString(scan, "Enter Person 2's Last Name:");
+        int p3Age = InputHelper.getRangedInt(scan, "Get Person 3's Age: [1-99]", 1, 99);
+        int p3Health = InputHelper.getRangedInt(scan, "Enter Person 3's HP:",0,100);
+        int p3Direction = InputHelper.getRangedInt(scan, "Enter Person 3's Direction:",1,6);
+        int p3X = InputHelper.getRangedInt(scan, "Enter Person 3's X Coordinate:",0,1000);
+        int p3Y = InputHelper.getRangedInt(scan, "Enter Person 3's Y Coordinate:",0,100);
+        int p3Z = InputHelper.getRangedInt(scan, "Enter Person 3's Z Coordinate:",0,1000);
 
         //Creating Person Objects
-        Person p1 = new Person(p1FirstName, p1lastName, p1Age);
-        Person p2 = new Person(p2FirstName, p2lastName, p2Age);
+        Person p1 = new Person(p1FirstName);
+        Person p2 = new Person(p2FirstName, p2LastName, p2X, p2Y, p2Z);
+        Person p3 = new Person(p3FirstName, p3LastName, p3Age, p3X, p3Y, p3Z);
 
         //Displaying the info using toString
         System.out.println("Person 1 Info\n" + p1);
         System.out.println("Person 2 Info\n" + p2);
+        System.out.println("Person 2 Info\n" + p3);
 
         //Updating Info
         p2.setLastName("Noob");
@@ -29,5 +41,6 @@ public class Main {
         //Redisplaying Info
         System.out.println("Person 1 Info\n" + p1);
         System.out.println("Person 2 Info\n" + p2);
+        System.out.println("Person 2 Info\n" + p3);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -3,16 +3,49 @@ public class Person {
     private String lastName;
     private String firstName;
     private int age;
+    private int hp;
+    private int direction;
+    private int x;
+    private int y;
+    private int z;
+
+    private int numPlayers = 0;
 
     //Constructor
-    public Person (String firstName, String lastName, int age){
+    public Person(String firstName){
+        this.firstName = "P";
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.direction = 1;
+        this.hp = 20;
+        numPlayers++;
+    }
+
+    public Person(String firstName, String lastName, int x, int y, int z){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = 1;
+        this.hp = 20;
+        numPlayers++;
+    }
+    public Person (String firstName, String lastName, int age, int hp, int direction, int x, int y, int z){
         this.lastName = lastName;
         this.firstName = firstName;
         this.age = age;
+        this.hp = hp;
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        numPlayers++;
     }
 
     public String toString() {
-        return String.format("First name: %s Last Name: %s Age: %d", this.firstName, this.lastName, this.age);
+        return String.format("First name: %s Last Name: %s Age: %d Health : %d", this.firstName, this.lastName, this.age, this.hp, this.direction, this.x, this.y, this.z);
     }
 
     //Accessors
@@ -28,6 +61,9 @@ public class Person {
         return age;
     }
 
+    public int getHp(){ return hp;}
+
+    public int getDirection(){ return direction;}
 
     //Mutators
     public void setFirstname(String firstName){
@@ -41,4 +77,6 @@ public class Person {
     public void setAge(int age){
         this.age = age;
     }
+
+    public
 }
